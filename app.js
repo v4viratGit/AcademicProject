df0fc1866ddf812eacc4a5c32b90c5bd35f93173
Virat Kamble <viratkamble19@gmail.com>
2020-12-19 23:27:04 +0530

    VK Improved SP's work, completed User CRUD, embedded admin into user, Implemented dynamic nav bar options

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 
     //EXPRESS
 const express       =       require("express"),
-      app           =       express(),
-      bodyParser    =       require("body-parser");
-      app.use(bodyParser.urlencoded({extended: true}));
+      app           =       express();
       app.use(express.urlencoded({extended:true}));
 
     //EJS
@@ -50,6 +48,16 @@ const express       =       require("express"),
 
     //MIDDLEWARE
     middleware = require("./middleware/middleware");
+
+    //RES.LOCALS AND FLASH
+    const flash         = require("connect-flash");
+    app.use(flash());
+    app.use((req,res,next)=> {
+        res.locals.currentUser=req.user;
+        res.locals.errorFlash=req.flash("error");
+        res.locals.successFlash=req.flash("success");
+        next(); 
+     });
 //----------------------------------------------------------------------//
 
 
@@ -57,13 +65,13 @@ const express       =       require("express"),
 //ROUTES
     //HOME ROUTE
     app.get("/", (req,res)=>{
-        res.render("home", {allstaff: null});
+        res.render("home");
     });
 
     //----------------------//
     //CRUD FOR USER
+
     //CREATE NEW USER
-    //USER SIGNUP
     //USER SIGNUP FORM GET ROUTE
     app.get("/user/new", (req,res)=>{
         res.render("signup-form");
@@ -83,7 +91,7 @@ const express       =       require("express"),
                 console.log(err);
             }
             passport.authenticate("local")(req, res, function(){
-                res.redirect("/login");
+                res.redirect("/");
             });
         });  
     });
@@ -94,24 +102,93 @@ const express       =       require("express"),
         res.render("login-form");
     });
     //LOGIN FORM HANDLER POST ROUTE
-    app.post("/user/login", passport.authenticate('local', { failureRedirect: '/login',
+    app.post("/user/login", passport.authenticate('local', { failureRedirect: '/',
     failureFlash: true }),(req,res)=>{
         res.redirect("/");
     });
 
-    //ADMIN LOGIN
-    //ADMIN LOGIN FORM GET ROUTE
-    app.get("/admin/login", (req,res)=>{
-        res.render("admin-login");
-    });
-    //LOGIN FORM HANDLER POST ROUTE
-    app.post("/admin/login", passport.authenticate('local', { failureRedirect: '/login',
-    failureFlash: true }),(req,res)=>{
-        res.redirect("/admin");
+    //USER LOGOUT
+    app.get("/user/logout", function (req,res) {
+        req.logout(); 
+        res.redirect("/");
+     });
+
+     //FIND, READ, AND SHOW USER
+     app.get("/user/:id", (req,res)=>{
+        User.findById(req.params.id,(err, foundUser)=>{
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("user-account", {foundUser});
+          }
+        });
+      });
+
+      //EDIT AND UPDATE USER INFORMATION
+      //EDIT USER GET ROUTE
+      app.get("/user/:id/edit", (req,res)=>{
+        const id=req.params.id;
+        User.findById(id, (err,foundUser)=>{
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("edit-profile", {foundUser});
+          }
+        });
+      });
+  //UPDATE USER PUT ROUTE
+      app.put("/user/:id",(req,res)=>{
+        const editedUser={
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email,
+            phone: req.body.phone,
+            address: req.body.address,
+        };
+        User.findByIdAndUpdate(req.params.id, editedUser, (err, updatedUser)=>{
+            if (err) {
+              console.log(err);
+            } else {
+              res.redirect("/");
+            }
+          });
+      });
+
+      //DELETE USER DELETE ROUTE
+      app.delete("/user/:id", (req,res)=>{
+        User.findByIdAndDelete(req.params.id,(err)=>{
+          if (err) {
+            console.log(err);
+          } else {
+            res.redirect("/")
+          }
+        }
+        );
+      });
+
+    //--------------------------------------------//
+
+    //----------------------------//
+    //ADMIN ROUTES
+    //ADMIN DASHBOARD
+    //ROUTE TO SHOW ALL ORDERS TO THE ADMIN
+    app.get("/admin/orders", (req,res,next)=>{
+        if(req.isAuthenticated()&&req.user._id=="5fddfb41e8e4af22f064ede8"){
+            return next();
+        }    
+        res.redirect("/login");
+        },(req,res)=>{
+        Order.find({}, function(err, allOrders){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("admin", {allorders: allOrders});
+            }
+        });
     });
 
 
-    //ORDER MAID ROUTE
+    //ORDER ROUTE
     app.get("/order", middleware.isLoggedIn, (req,res)=>{
         Staff.find({}, function(err, allStaff){
             if(err){
@@ -135,17 +212,6 @@ const express       =       require("express"),
             console.log(err);
         });
     });    
-
-    //SHOW ORDERS ROUTE
-    app.get("/showOrders", (req,res)=>{
-        Order.find({}, function(err, allOrders){
-            if(err){
-                console.log(err);
-            } else {
-                res.render("admin", {allorders: allOrders});
-            }
-        });
-    });
 
     //LOGIN FORM ROUTE
     app.get("/login", (req,res)=>{
@@ -191,10 +257,7 @@ const express       =       require("express"),
         });
     });
 
-    //ADMIN PAGE ROUTE
-    app.get("/admin", middleware.isLoggedIn, (req,res)=>{
-        res.render("admin", {allorders: null});
-    });
+ 
 
 
 //PORT LISTENER
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,15 +1,13 @@
 //MIDDLEWARE OBJECT
 const middlewareObj={};
 
-
 //MIDDLEWARE TO CHECK IF THE USER IS LOGGED IN OR NOT
-middlewareObj.isLoggedIn = function (req, res, next) {
+middlewareObj.isLoggedIn = (req, res, next)=> {
     if(req.isAuthenticated()){
         return next();
     }    
     res.redirect("/login");
 }
-
 
 //EXPORT THE MIDDLEWARE OBJECT FOR USER
 module.exports=middlewareObj
